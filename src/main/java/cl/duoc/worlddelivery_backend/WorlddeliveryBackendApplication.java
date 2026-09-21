2bcefad58a27324c7c2b8e4e11f7bbe5e18ee1cf
Matias <mabre@Mabre.Mabre>
2026-09-21 12:00:59 -0300

    Primer commit: Codigo base del BackEnd

---
+++ src/main/java/cl/duoc/worlddelivery_backend/WorlddeliveryBackendApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.worlddelivery_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorlddeliveryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorlddeliveryBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/duoc/worlddelivery_backend/WorlddeliveryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.worlddelivery_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorlddeliveryBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
